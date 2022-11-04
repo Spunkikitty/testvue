0e437d3e66f0097379a1b70c94f15cb324820425
Ebony Jackson <travelbeanie@gmail.com>
2022-11-04 17:34:56 -0400

    images C

---
+++ Chapter-07-Frontend/Groupmania.vue
@@ -0,0 +1,23 @@
+<template> 
+<div id=" app">
+ <div :style="{'background-image':'url(./assets/images/icon-above-front.png)'}">
+  </div>
+</div>
+</template>
+
+<script>
+ export default 
+    
+       
+
+
+</script>
+
+<style>
+ .background-image{ 
+  background-image: center;
+  
+ }
+
+</style>
+
