5c19321511bdb6303fd18e7fc133694b2fc23561
Ebony Jackson <travelbeanie@gmail.com>
2022-11-04 19:04:34 -0400

    update D

--- Chapter-07-Frontend/Groupmania.vue
+++ Chapter-07-Frontend/Groupmania.vue
@@ -1,14 +1,38 @@
+import vue from 'Vue',
+import app from 'App',
 <template> 
 <div id=" app">
+  <h1>Welcome to Groupmania</h1>
+   
  <div :style="{'background-image':'url(./assets/images/icon-above-front.png)'}">
   </div>
 </div>
+<form class="form-1">
+<ul> 
+  <li>FirstName ${{FirstName}}</li>
+  <li>LastName ${{LastName}}</li>
+  <li>Email ${{Email}}</li>
+  <li>Password ${{Password}}</li>
+</ul>
+</form>
+
 </template>
 
 <script>
- export default 
-    
-       
+export default{
+  const app = new Vue({
+      el: '#app',
+      data: { 
+        FirstName,
+        LastName, 
+        Email,
+        Password
+      }
+      })
+      <form @submit.prevent ="onSubmit">
+      <button@click = "(event) => warn('Use Company Email Only.', event)"> Submit </button>
+      </form>
+
 
 
 </script>
@@ -16,8 +40,12 @@
 <style>
  .background-image{ 
   background-image: center;
-  
- }
+  };
+  .form-1{
+    
+    background-color: #ffd7d7;
+
+  }; 
 
 </style>
 
