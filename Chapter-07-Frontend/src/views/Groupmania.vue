858e8b42066426b12383afe19d6e81f791f68c82
Ebony Jackson <travelbeanie@gmail.com>
2022-11-04 20:14:29 -0400

    update D

---
+++ Chapter-07-Frontend/src/views/Groupmania.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <div id="app">
+    <h1>Welcome to Groupmania</h1>
+
+    <div :style="{ 'background-image': 'url(./assets/images/icon-above-front.png)' }">
+    </div>
+  </div>
+  <form class="form-1" @submit.prevent="onSubmit">
+    <ul>
+      <li>FirstName ${{ FirstName }}</li>
+      <li>LastName ${{ LastName }}</li>
+      <li>Email ${{ Email }}</li>
+      <li>Password ${{ Password }}</li>
+    </ul>
+
+    <button @click="(event) => warn('Use Company Email Only.', event)"> Submit </button>
+  </form>
+
+</template>
+
+<script>
+
+
+export default {
+  name: "GroupmaniaView",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: ""
+    }
+  }
+}
+
+
+
+
+</script>
+
+<style>
+.background-image {
+  background-image: center;
+}
+
+;
+
+.form-1 {
+  background-color: #ffd7d7;
+
+}
+
+;
+</style>
+
